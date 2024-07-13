861f30110172265e47189fad470f38498e73ed4c
Matthew Bolofinde <molanrewajuking@gmail.com>
2024-07-13 16:59:45 -0500

    added fib function

---
+++ fibo_return.py
@@ -0,0 +1,11 @@
+# Writing a Fibonacci Series that returns the values
+
+def fib_return(n):
+    """ Returns a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
