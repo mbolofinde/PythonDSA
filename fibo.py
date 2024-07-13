b60145334b26a3bdf5b7c7c0fda9ed0762f7850a
Matthew Bolofinde <molanrewajuking@gmail.com>
2024-07-13 16:45:30 -0500

    updaetd fibo file

--- fibo.py
+++ fibo.py
@@ -1,1 +1,12 @@
 # Write Fibonacci series up to n 
+# Write a function and pass the desired series
+
+def fib(n):
+    a, b = 0, 1
+    # define the conditon
+    while a < n:
+        print(a, end=',')
+        a, b = b, a+b
+    print()
+
+
